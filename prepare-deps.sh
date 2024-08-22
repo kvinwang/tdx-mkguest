002849be1d5e8c8abf71461e37cd368b7e8775a6
Kevin Wang <wy721@qq.com>
2024-08-22 01:40:15 +0000

    Fix mount bugs

--- prepare-deps.sh
+++ prepare-deps.sh
@@ -3,7 +3,14 @@
 apt update
 
 # install required tools
-apt install --yes qemu-utils libguestfs-tools virtinst genisoimage libvirt-daemon-system nbdfuse fuse2fs
+apt install --yes qemu-utils libguestfs-tools virtinst genisoimage libvirt-daemon-system
+
+# rootfs tools
+apt install --yes qemu-utils nbdfuse fuse2fs
+
+# lxc tools
+apt install --yes lxc-utils
+
 # to allow virt-customize to have name resolution, dhclient should be available
 # on the host system. that is because virt-customize will create an appliance (with supermin)
 # from the host system and will collect dhclient into the appliance
